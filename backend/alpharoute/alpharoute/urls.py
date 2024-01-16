bbcd2bb47d431bede110b832e568df677e44b2b6
Евгений Жуков <zhukov1414@yandex.ru>
2024-01-16 20:43:42 +0300

    startproject

---
+++ backend/alpharoute/alpharoute/urls.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="AlphaBank API",
+        default_version="v1",
+        description="API Documentation ",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    path(
+        "swagger<format>/",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+]
