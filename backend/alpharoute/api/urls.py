0240fa0fec0a14147fd4021a0f246c19b45a899d
Евгений Жуков <zhukov1414@yandex.ru>
2024-01-16 21:31:47 +0300

    add urls.py for api

---
+++ backend/alpharoute/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+router_v1 = DefaultRouter()
+
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+]
