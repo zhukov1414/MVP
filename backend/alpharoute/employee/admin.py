b72b503eac8729e841fcd3d613a0ed5d90a0e0fc
NikiSv <svetanim@gmail.com>
2024-01-19 16:12:23 +0700

    Creating user list router

--- backend/alpharoute/employee/admin.py
+++ backend/alpharoute/employee/admin.py
@@ -1,21 +1,13 @@
 from django.contrib import admin
 
-from .models import Employee, CustomUser
+from .models import CustomUser
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'role')
-    list_filter = ('username', 'email', 'role')
+    list_display = ('username', 'first_name', 'last_name', 'position')
+    list_filter = ('username', 'email', 'position', 'is_staff')
     search_fields = ('username',)
     empty_value_display = '-пусто-'
 
 
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('user', 'position',)
-    list_filter = ('user',)
-    search_fields = ('user',)
-    empty_value_display = '-пусто-'
-
-
-admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(CustomUser, CustomUserAdmin)
