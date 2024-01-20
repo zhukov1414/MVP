989422559c3d063390d58eaa00975e9c08255bb9
NikiSv <svetanim@gmail.com>
2024-01-20 20:06:54 +0700

    Creating root template

--- backend/alpharoute/api/serializers.py
+++ backend/alpharoute/api/serializers.py
@@ -1,6 +1,8 @@
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
+
 from employee.models import CustomUser
+from templatestask.models import Department, Template
 
 
 class CustomUserSerializer(UserSerializer):
@@ -29,3 +31,20 @@ class CustomUserListSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'first_name', 'last_name', 'position',)
         read_only_fields = ('id', 'name', 'first_name',
                             'last_name', 'position',)
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    """Сериализация направления для шаблона."""
+
+    class Meta:
+        model = Department
+        fields = ('id', 'title')
+
+
+class TemplateSerializer(serializers.ModelSerializer):
+    """Сериализация для создания шаблона."""
+
+    class Meta:
+        model = Template
+        fields = ('id', 'title', 'description', 'linkURL', 'department')
+        # шаблон привязан к направлению?
